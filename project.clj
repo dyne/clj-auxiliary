40d45697f11fab5bb37b45c67f15baeccb2a7861
Jaromil <jaromil@dyne.org>
2019-03-19 15:55:57 +0100

    updated to clojure 10
    fix for jdk10

--- project.clj
+++ project.clj
@@ -3,10 +3,17 @@
   :url "https://dyne.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.forward/yaml "1.0.6"]
+
+  ;; fix for jdk 10
+  :managed-dependencies [[org.clojure/core.rrb-vector "0.0.14"]
+                       [org.flatland/ordered "1.5.7"]]
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [io.forward/yaml "1.0.9"]
                  ;; logs
                  [com.taoensso/timbre "4.10.0"]
                  ;; env vars used for translation
                  [environ "1.1.0"]]
-  :profiles {:dev {:plugins [[lein-marginalia "0.9.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.6"]]
+                   :plugins [[lein-midje "3.1.3"]
+                             [lein-marginalia "0.9.0"]]}})
